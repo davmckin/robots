e3a9f07f5a8afc24e59ca5aec87014fe9421d7b3
DAVMCKIN <DAVIDJMCKIN@GMAIL.COM>
2017-02-01 15:56:36 -0500

    legendary

--- robots.rb
+++ robots.rb
@@ -1,5 +1,4 @@
 class Robot
-
   attr_accessor :name, :height
   def initialize(name, height = 10)
     @name = name
@@ -7,7 +6,7 @@ class Robot
   end
 
   def say_hi
-    puts "Hi"
+    puts 'Hi'
   end
 
   def say_name
@@ -15,27 +14,30 @@ class Robot
   end
 end
 class Bendingunit < Robot
-
   def Bend(object_to_bend)
     puts "Bend #{object_to_bend}"
   end
 end
 
 class ActorUnit < Robot
-    def change_name(new_name)
-      @name = new_name
-    end
+  def change_name(new_name)
+    @name = new_name
+  end
 end
 
 roboclass = []
 
-our_class = ["Rob", "David", "Nancy", "Kalea", "Laura", "Dave", "Demetra", "Kendrick", "Phil", "Ben", "Michael", "Miguel"]
+our_class = %w(Rob David Nancy Kalea Laura Dave Demetra Kendrick Phil Ben Michael Miguel)
 our_class.each do |robo|
   roboclass << Robot.new(robo)
 end
 
 randobo = [Robot, Bendingunit, ActorUnit]
 
-roboclass = our_class.collect { |assign|
-   randobo.sample.new(assign)}
-   puts roboclass.inspect
+roboclass = our_class.collect do |assign|
+  randobo.sample.new(assign)
+end
+
+def method_missing(m, *_args)
+  puts "There's no method called #{m} here -- please try again."
+end
